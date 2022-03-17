070341eb31b900a0c7c1b038dfb897126d65ce28
Jeef-Jr <jefferson.goncalvesfo@sptech.school>
2022-03-16 22:10:22 -0300

    Database Version Final

--- database/graotech.sql
+++ database/graotech.sql
@@ -1,12 +1,11 @@
 CREATE DATABASE graotech;
 Use graotech;
 
-CREATE TABLE users (
+CREATE TABLE user (
 id int primary key auto_increment,
 nome varchar(50),
 sobrenome varchar(50),
 cpf varchar(20),
-cnpj varchar(20),
 telefone varchar(20),
 email varchar(70),
 endereco varchar(225),
@@ -16,40 +15,34 @@ senha varchar(255),
 criado date
 );
 
-CREATE TABLE sensores (
-id_sensor int primary key auto_increment,
-user_id int,
-max_temp float,
-min_temp float,
-max_umi float,
-min_umi float
-);
-
-
-CREATE TABLE status (
-id_st int primary key auto_increment,
-id_sensor int,
-tipo varchar(50),
-atual_temp float,
-atual_umi float,
+CREATE TABLE sensore (
+id int primary key auto_increment,
+nome varchar(100),
+status varchar(100),
+localidade varchar(100),
+temp_atual float,
+umi_atual float,
 date_time datetime
 );
 
-CREATE TABLE alertas(
-	id_alert int primary key auto_increment,
-    id_sensor int,
-    nome varchar(50),
-    tipo varchar(50),
-    graus float,
-    date_time datetime
-);
 
-
-CREATE TABLE produtos(
-id_produto int primary key auto_increment,
-id_sensor int,
+CREATE TABLE produto(
+id int primary key auto_increment,
 nome varchar(100),
-quantidade int
+temp_max float,
+temp_min float,
+umi_max float,
+umi_min float,
 );
+
+CREATE TABLE alerta(
+id int primary key auto_increment,
+nome varchar(50),
+tipo varchar(50),
+graus float,
+date_time datetime
+);
+
+
 
 
